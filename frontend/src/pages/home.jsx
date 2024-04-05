9a873408a6368b4d88a108ef3c65518ba3b41a4d
JoseRicardo0110 <165341012+JoseRicardo0110@users.noreply.github.com>
2024-04-05 15:20:07 -0500

    este es mi cambio

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
             <Navbar />
             <h2>HOLA SOY LA HOME PAGE</h2>
             <h1>Jose angel q pasaaa</h1>
+            <h1>soy yo</h1>
         </>
     )
 }
